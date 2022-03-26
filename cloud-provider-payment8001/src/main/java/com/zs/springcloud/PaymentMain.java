6b1040c4caf50db0454993784edba0316f6d2dea
zhouson0907 <1061674782@qq.com>
2022-03-26 16:20:04 +0800

    支付模块基本构建

---
+++ cloud-provider-payment8001/src/main/java/com/zs/springcloud/PaymentMain.java
@@ -0,0 +1,15 @@
+package com.zs.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouson
+ * @create 2022-03-26 15:56
+ */
+@SpringBootApplication
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}
